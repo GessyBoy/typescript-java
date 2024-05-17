7bb3d4776992f1007feb6cb8bef4268cf9721cf4
GessyBoy <ydeletrez@gmail.com>
2024-05-17 08:30:26 +0200

    edit change typescript file in to java file

---
+++ Photo.java
@@ -0,0 +1,39 @@
+public class Photo {
+    private String url;
+    private String filename;
+    private int width;
+    private int height;
+
+    // Getters and Setters
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
